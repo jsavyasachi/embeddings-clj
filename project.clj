16874397adf82b951381aae5cee73d351b6ee870
Savyasachi <jsavyasachi@gmail.com>
2026-07-16 00:30:17 -0700

    chore(release): 0.3.3

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject net.clojars.savya/embeddings-clj "0.3.3"
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
